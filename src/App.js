b832c67ced41ee76b46db640c719eee870125e6e
tapia1013 <manbearpig1013@gmail.com>
2020-07-03 21:56:31 -0700

    added router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 
-import HomePage from './pages/homepage/homepage.component'
+
+const HatsPage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
+// video react router at 7:40s
